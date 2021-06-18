25a9bb799e6d7e23a2a0b2fd21069a33da6fd4db
Andrew Kane <andrew@ankane.org>
2021-06-17 18:47:01 -0700

    Added test for SQLx

--- src/sqlx_ext.rs
+++ src/sqlx_ext.rs
@@ -51,7 +51,30 @@ impl Decode<'_, Postgres> for Vector {
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
+    #[async_std::test]
+    async fn it_works() -> Result<(), sqlx::Error> {
+        use crate::Vector;
+        use sqlx::postgres::PgPoolOptions;
+        use sqlx::Row;
+
+        let pool = PgPoolOptions::new()
+            .max_connections(1)
+            .connect("postgres://localhost/pgvector_rust_test").await?;
+
+        sqlx::query("CREATE EXTENSION IF NOT EXISTS vector").execute(&pool).await?;
+        sqlx::query("DROP TABLE IF EXISTS t").execute(&pool).await?;
+        sqlx::query("CREATE TABLE t (id bigserial primary key, c vector(3))").execute(&pool).await?;
+
+        let vec = Vector::from(vec![1.0, 2.0, 3.0]);
+        let vec2 = Vector::from(vec![4.0, 5.0, 6.0]);
+        sqlx::query("INSERT INTO t (c) VALUES ($1), ($2), (NULL)").bind(&vec).bind(&vec2).execute(&pool).await?;
+
+        let query_vec = Vector::from(vec![3.0, 1.0, 2.0]);
+        let row = sqlx::query("SELECT c from t ORDER BY c <-> $1 LIMIT 1").bind(query_vec).fetch_one(&pool).await?;
+        let res_vec: Vector = row.try_get("c").unwrap();
+        assert_eq!(vec, res_vec);
+        assert_eq!(vec![1.0, 2.0, 3.0], res_vec.to_vec());
+
+        Ok(())
     }
 }
