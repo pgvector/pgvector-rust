2803df27a1a72aa9be391a30b5c1edd3c01feaae
Andrew Kane <andrew@ankane.org>
2023-08-12 01:30:41 -0700

    Improved example code [skip ci]

--- examples/disco/src/main.rs
+++ examples/disco/src/main.rs
@@ -3,53 +3,55 @@ use discorec::{Dataset, RecommenderBuilder};
 use pgvector::Vector;
 use postgres::{Client, NoTls};
 use std::collections::HashMap;
+use std::error::Error;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     // https://grouplens.org/datasets/movielens/100k/
-    let movielens_path = std::env::var("MOVIELENS_100K_PATH").expect("Set MOVIELENS_100K_PATH");
+    let movielens_path = std::env::var("MOVIELENS_100K_PATH").or(Err("Set MOVIELENS_100K_PATH"))?;
 
     let mut client = Client::configure()
         .host("localhost")
         .dbname("pgvector_rust_test")
-        .user(std::env::var("USER").unwrap().as_str())
-        .connect(NoTls)
-        .unwrap();
+        .user(std::env::var("USER")?.as_str())
+        .connect(NoTls)?;
 
-    client.execute("CREATE EXTENSION IF NOT EXISTS vector", &[]).unwrap();
-    client.execute("DROP TABLE IF EXISTS users", &[]).unwrap();
-    client.execute("DROP TABLE IF EXISTS movies", &[]).unwrap();
-    client.execute("CREATE TABLE users (id integer PRIMARY KEY, factors vector(20))", &[]).unwrap();
-    client.execute("CREATE TABLE movies (name text PRIMARY KEY, factors vector(20))", &[]).unwrap();
+    client.execute("CREATE EXTENSION IF NOT EXISTS vector", &[])?;
+    client.execute("DROP TABLE IF EXISTS users", &[])?;
+    client.execute("DROP TABLE IF EXISTS movies", &[])?;
+    client.execute("CREATE TABLE users (id integer PRIMARY KEY, factors vector(20))", &[])?;
+    client.execute("CREATE TABLE movies (name text PRIMARY KEY, factors vector(20))", &[])?;
 
     let data = load_movielens(Path::new(&movielens_path));
     let recommender = RecommenderBuilder::new().factors(20).fit_explicit(&data);
 
     for user_id in recommender.user_ids() {
         let factors = Vector::from(recommender.user_factors(user_id).unwrap().to_vec());
-        client.execute("INSERT INTO users (id, factors) VALUES ($1, $2)", &[&user_id, &factors]).unwrap();
+        client.execute("INSERT INTO users (id, factors) VALUES ($1, $2)", &[&user_id, &factors])?;
     }
 
     for item_id in recommender.item_ids() {
         let factors = Vector::from(recommender.item_factors(item_id).unwrap().to_vec());
-        client.execute("INSERT INTO movies (name, factors) VALUES ($1, $2)", &[&item_id, &factors]).unwrap();
+        client.execute("INSERT INTO movies (name, factors) VALUES ($1, $2)", &[&item_id, &factors])?;
     }
 
     let movie = "Star Wars (1977)";
     println!("Item-based recommendations for {}", movie);
-    for row in client.query("SELECT name FROM movies WHERE name != $1 ORDER BY factors <=> (SELECT factors FROM movies WHERE name = $1) LIMIT 5", &[&movie]).unwrap() {
+    for row in client.query("SELECT name FROM movies WHERE name != $1 ORDER BY factors <=> (SELECT factors FROM movies WHERE name = $1) LIMIT 5", &[&movie])? {
         let name: &str = row.get(0);
         println!("- {}", name);
     }
 
     let user_id = 123;
     println!("\nUser-based recommendations for user {}", user_id);
-    for row in client.query("SELECT name FROM movies ORDER BY factors <#> (SELECT factors FROM users WHERE id = $1) LIMIT 5", &[&user_id]).unwrap() {
+    for row in client.query("SELECT name FROM movies ORDER BY factors <#> (SELECT factors FROM users WHERE id = $1) LIMIT 5", &[&user_id])? {
         let name: &str = row.get(0);
         println!("- {}", name);
     }
+
+    Ok(())
 }
 
 fn load_movielens(path: &Path) -> Dataset<i32, String> {
