e79487c2665b7555feddc73f57eb95315b97728f
Andrew Kane <andrew@ankane.org>
2024-05-19 10:51:22 -0400

    Improved bit code [skip ci]

--- src/postgres_ext/bit.rs
+++ src/postgres_ext/bit.rs
@@ -19,7 +19,7 @@ impl ToSql for Bit {
     fn to_sql(&self, _ty: &Type, w: &mut BytesMut) -> Result<IsNull, Box<dyn Error + Sync + Send>> {
         let len = self.len;
         w.put_i32(len.try_into()?);
-        w.put(&*self.data);
+        w.put(&self.data[..]);
         Ok(IsNull::No)
     }
 
